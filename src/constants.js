643e5ba0e0fad6562a7a36940dbd8363b5647622
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-07 20:54:07 +0700

    update constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const ARTICLE_URI_PREFIX = 'nyt://article/';
-export const HOME_PAGE_API_URL = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+export const IMAGE_URL_PREFIX = 'https://static01.nyt.com/';
 export const NYTIMES_API_KEY = '5W2RAEaPRU1Fu4dGH5wj9wIb4ozZn5BC';
+
+export const TOP_STORY_API_URL = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+export const SEARCH_API_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
