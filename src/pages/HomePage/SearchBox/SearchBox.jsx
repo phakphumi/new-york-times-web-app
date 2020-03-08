2551f9ac5cc1b3760fcb6ec9fb6977cbd888e8b6
pakpoom <pakpoom.t@student.chula.ac.th>
2020-03-08 15:27:26 +0700

    SearchBox should belongs to HomePage

--- src/pages/HomePage/SearchBox/SearchBox.jsx
+++ src/pages/HomePage/SearchBox/SearchBox.jsx
@@ -2,11 +2,16 @@ import { SearchOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
 import React from 'react';
 
+import { useSearchBox } from './SearchBoxHooks';
+
 export default function SearchBox() {
+  const { handleSearchTermChange } = useSearchBox();
+
   return (
     <Input
       placeholder="Search your article"
       prefix={<SearchOutlined />}
+      onChange={handleSearchTermChange}
     />
   );
 }
